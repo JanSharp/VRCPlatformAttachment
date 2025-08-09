d63328bbf61fcb1748b90a739cfce125df27889c
JanSharp <steinmeister36@gmail.com>
2025-08-09 18:24:48 +0200

    Add test tram track to dev world, and a lag switch
    Add simple tram animation sync, which is likely going to get out of sync
      as the animation plays out differently for people, that is what the
      lag switch is for, to test animation syncing

---
+++ Runtime/Dev/LagSwitch.cs
@@ -0,0 +1,55 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class LagSwitch : UdonSharpBehaviour
+    {
+        [HideInInspector][SerializeField][SingletonReference] private WidgetManager widgetManager;
+        public GenericValueEditor valueEditor;
+
+        private ToggleFieldWidgetData toggleWidget;
+        private SliderFieldWidgetData fpsSliderWidget;
+        private SliderFieldWidgetData lagSpikeSliderWidget;
+        private System.Diagnostics.Stopwatch stopwatch;
+
+        public void Start()
+        {
+            toggleWidget = widgetManager.NewToggleField("Lag Switch", false);
+            fpsSliderWidget = widgetManager.NewSliderField("FPS", 30f, 1f, 120f);
+            lagSpikeSliderWidget = widgetManager.NewSliderField("Lag Spike Seconds", 5f, 0f, 9f);
+            valueEditor.Draw(new WidgetData[]
+            {
+                toggleWidget,
+                fpsSliderWidget,
+                widgetManager.NewSpace(),
+                widgetManager.NewButton("Lag Spike")
+                    .SetListener(this, nameof(OnLagSpikeClick))
+                    .StdMoveWidget(),
+                lagSpikeSliderWidget,
+            });
+            stopwatch = new System.Diagnostics.Stopwatch();
+        }
+
+        public void Update()
+        {
+            if (!toggleWidget.Value)
+                return;
+            LagSpike(1d / fpsSliderWidget.Value);
+        }
+
+        public void OnLagSpikeClick()
+        {
+            LagSpike(lagSpikeSliderWidget.Value);
+        }
+
+        private void LagSpike(double seconds)
+        {
+            stopwatch.Reset();
+            stopwatch.Start();
+            while (stopwatch.Elapsed.TotalSeconds < seconds)
+                ; // Busy wait.
+        }
+    }
+}
---
+++ Runtime/Dev/TramAnimationSync.cs
@@ -0,0 +1,49 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon.Common;
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class TramAnimationSync : UdonSharpBehaviour
+    {
+        public Animator animator;
+
+        [UdonSynced] private int syncedTagHash;
+        [UdonSynced] private float syncedLength;
+        [UdonSynced] private float syncedNormalizedTime;
+
+        public void Start()
+        {
+            if (Networking.IsOwner(this.gameObject))
+                RequestSerialization();
+        }
+
+        private int lastProgressUpdate = 0;
+        public void Update()
+        {
+            var info = animator.GetCurrentAnimatorStateInfo(0);
+            int progress = (int)((info.normalizedTime % 1f) * 10f);
+            if (progress == lastProgressUpdate)
+                return;
+            lastProgressUpdate = progress;
+            Debug.Log($"<dlt> Tram animation normalizedTime: {lastProgressUpdate / 10f}");
+        }
+
+        public override void OnPreSerialization()
+        {
+            var info = animator.GetCurrentAnimatorStateInfo(0);
+            syncedTagHash = info.tagHash;
+            syncedLength = info.length;
+            syncedNormalizedTime = info.normalizedTime % 1f;
+        }
+
+        public override void OnDeserialization(DeserializationResult result)
+        {
+            float transitTime = result.receiveTime - result.sendTime;
+            float transitNormalizedTime = transitTime / syncedLength;
+            animator.Play(syncedTagHash, 0, (syncedNormalizedTime + transitNormalizedTime) % 1f);
+        }
+    }
+}
